04bd18260133e1fdb4fba101a4f6d45be8eef1c5
Julián González Calderón <gonzalezcalderonjulian@gmail.com>
2023-12-26 17:07:39 -0300

    Final fixes

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,30 +1,26 @@
 use itertools::Itertools;
 
-struct Range {
-    min: usize,
-    max: usize,
-}
+struct Range(usize, usize);
 
 impl Range {
     pub fn contains(&self, other: &Self) -> bool {
-        self.min <= other.min && self.max >= other.max
+        self.0 <= other.0 && self.1 >= other.1
     }
 
     fn overlaps(&self, other: &Range) -> bool {
-        self.min <= other.max && self.max >= other.min
+        self.0 <= other.1 && self.1 >= other.0
     }
 }
 
 impl From<&str> for Range {
     fn from(value: &str) -> Self {
         let (min, max) = value
-            .split_once("-")
-            .expect("There was no bounds separator");
+            .splitn(2, "-")
+            .flat_map(str::parse::<usize>)
+            .collect_tuple()
+            .expect("Range was malformed");
 
-        Self {
-            min: min.parse().expect("Left bound was not integer"),
-            max: max.parse().expect("Right bound was not integer"),
-        }
+        Self(min, max)
     }
 }
 
@@ -36,12 +32,15 @@ fn part1(data: String) -> usize {
         .count()
 }
 
-fn parse_ranges(pair: &str) -> Vec<Range> {
-    pair.split(",").map(Range::from).collect_vec()
+fn parse_ranges(pair: &str) -> (Range, Range) {
+    pair.split(",")
+        .map(Range::from)
+        .collect_tuple()
+        .expect("Input was malformed")
 }
 
-fn has_containment(pair: &Vec<Range>) -> bool {
-    pair[0].contains(&pair[1]) || pair[1].contains(&pair[0])
+fn has_containment(pair: &(Range, Range)) -> bool {
+    pair.0.contains(&pair.1) || pair.1.contains(&pair.0)
 }
 
 fn part2(data: String) -> usize {
@@ -52,8 +51,8 @@ fn part2(data: String) -> usize {
         .count()
 }
 
-fn has_overlap(pair: &Vec<Range>) -> bool {
-    pair[0].overlaps(&pair[1])
+fn has_overlap(pair: &(Range, Range)) -> bool {
+    pair.0.overlaps(&pair.1)
 }
 
 aoc::main!(4);
